8242bf0aae74d2dbf5d722f15c89b10556e1d1f5
Jiaqing Chen <jchen501@asu.edu>
2021-04-30 12:31:43 -0700

    Add messages sending part

--- LivenessDetection/app/src/main/java/com/example/livenessdetection/GroupInputActivity.java
+++ LivenessDetection/app/src/main/java/com/example/livenessdetection/GroupInputActivity.java
@@ -4,64 +4,51 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.graphics.Color;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.OkHttpClient;
-import okhttp3.RequestBody;
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-public class GroupInputActivity extends AppCompatActivity{
+public class GroupInputActivity extends AppCompatActivity implements View.OnClickListener{
 
     private TextView tv_group_preprossing;
     private TextView tv_group_inferring;
-    private File file;
 
-    public void uploadVideo(View view) {
-        Toast.makeText(getApplicationContext(), "Start Uploading the group of datasets", Toast.LENGTH_LONG).show();
-        String base_url = "http://0.0.0.0:5000/";
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-        Retrofit.Builder retrofitBuilder = new Retrofit.Builder().baseUrl(base_url).addConverterFactory(GsonConverterFactory.create());
-        Retrofit retrofit = retrofitBuilder.client(httpClient.build()).build();
-        UploadHelper fileUtils = retrofit.create(UploadHelper.class);
-        String desc = "one group of datasets";
-        RequestBody description = RequestBody.create(MediaType.parse("multipart/form-data"), desc);
-        RequestBody requestFile = RequestBody.create(MediaType.parse("text/plain"), file);
-        MultipartBody.Part body = MultipartBody.Part.createFormData("file", file.getName(), requestFile);
-        Call<ResponseBody> call = fileUtils.uploadVideo(description, body);
-        call.enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                System.out.println(response.toString());
-                Log.e("mw", "Server_response: " + response.toString());
-            }
+    public static final int SERVERPORT = 8088;
 
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                System.out.println(t.toString());
-                Log.e("mw", "upload_err: " + t.toString());
-            }
-        });
-    }
+    public static final String SERVER_IP = "10.0.2.2";
+    private ClientThread clientThread;
+    private Thread thread;
+    private LinearLayout msgListG;
+    private Handler handler;
+    private int clientTextColor;
+    private TextView edMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groupinput);
+
+        clientTextColor = ContextCompat.getColor(this, R.color.black);
+        handler = new Handler();
+        msgListG = findViewById(R.id.msgListG);
+        edMessage = findViewById(R.id.textView8);
 
         tv_group_preprossing = (TextView)findViewById(R.id.textView7);
         String txt_preprossing = "Preprossing Result Shown Here";
@@ -110,7 +97,7 @@ public class GroupInputActivity extends AppCompatActivity{
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                tv_group_inferring.setText("Inferring Finished\nHere is the results:\nAccuracy:\nFalse accept rate:\nFalse reject rate:\nHalf total error:\nF1 score:");
+                                tv_group_inferring.setText("Inferring Finished");
                                 try {
                                     Thread.sleep(15000);
                                 } catch (InterruptedException e) {
@@ -131,5 +118,113 @@ public class GroupInputActivity extends AppCompatActivity{
                 startActivity(intent_main);
             }
         });
+    }
+
+    public TextView textView(String message, int color) {
+        if (null == message || message.trim().isEmpty()) {
+            message = "<Empty Message>";
+        }
+        TextView tv = new TextView(this);
+        tv.setTextColor(color);
+        tv.setText(message + " [" + getTime() + "]");
+        tv.setTextSize(20);
+        tv.setPadding(0, 5, 0, 0);
+        return tv;
+    }
+
+    public void showMessage(final String message, final int color) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                msgListG.addView(textView(message, color));
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.button6) {
+            msgListG.removeAllViews();
+            showMessage("Connecting to Server...", clientTextColor);
+            clientThread = new ClientThread();
+            thread = new Thread(clientThread);
+            thread.start();
+            showMessage("Connected to Server...", clientTextColor);
+            return;
+        }
+
+        if (view.getId() == R.id.button7) {
+            String clientMessage = edMessage.getText().toString().trim();
+            showMessage(clientMessage, Color.BLUE);
+            if (null != clientThread) {
+                clientThread.sendMessage(clientMessage);
+            }
+        }
+    }
+
+    class ClientThread implements Runnable {
+
+        private Socket socket;
+        private BufferedReader input;
+
+        @Override
+        public void run() {
+
+            try {
+                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+                socket = new Socket(serverAddr, SERVERPORT);
+
+                while (!Thread.currentThread().isInterrupted()) {
+
+                    this.input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String message = input.readLine();
+                    if (null == message || "Disconnect".contentEquals(message)) {
+                        Thread.interrupted();
+                        message = "Server Disconnected.";
+                        showMessage(message, Color.RED);
+                        break;
+                    }
+                    showMessage("Server: " + message, clientTextColor);
+                }
+
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+
+        void sendMessage(final String message) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        if (null != socket) {
+                            PrintWriter out = new PrintWriter(new BufferedWriter(
+                                    new OutputStreamWriter(socket.getOutputStream())),
+                                    true);
+                            out.println(message);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+    }
+
+    String getTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        return sdf.format(new Date());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != clientThread) {
+            clientThread.sendMessage("Disconnect");
+            clientThread = null;
+        }
     }
 }
